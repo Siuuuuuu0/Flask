24c99203b2f41a2ebdbe71beaf79445953948be6
Vadimk <vadimkotkin1855@gmail.com>
2024-07-26 00:03:56 +0200

    weather

--- weather-app/weather.py
+++ weather-app/weather.py
@@ -6,12 +6,16 @@ import requests
 load_dotenv()
 
 def get_current_weather(city="Moscow") :
-    reques_url = f"http://api.openweathermap.org/data/2.5/weather?appid={os.getenv('API_KEY')}&q={city}&units=metric"
+    request_url = f"http://api.openweathermap.org/data/2.5/weather?appid={os.getenv('API_KEY')}&q={city}&units=metric"
 
-    weather_data = requests.get(reques_url).json()
+    weather_data = requests.get(request_url).json()
 
     return weather_data
 
 if __name__ == "__main__" :
     city = input("Enter a city: ")
-    print(get_current_weather(city))
+
+    if not bool(city.strip()):
+        city = "Moscow"
+
+    pprint(get_current_weather(city))
